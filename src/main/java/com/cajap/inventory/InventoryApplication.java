3e1498c8b988d596255b1a19a7d2b3c3a88bb449
andrewbrianputosa <andrewbrianputosa@gmail.com>
2024-05-02 00:30:54 +0800

    first commit

--- src/main/java/com/cajap/inventory/InventoryApplication.java
+++ src/main/java/com/cajap/inventory/InventoryApplication.java
@@ -1,13 +1,20 @@
 package com.cajap.inventory;
 
+import org.slf4j.event.Level;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+//import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class InventoryApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws IOException {
 		SpringApplication.run(InventoryApplication.class, args);
+		Logger logger = Logger.getLogger("test");
+//		logger(Level.INFO,"test");
 	}
+
 
 }
