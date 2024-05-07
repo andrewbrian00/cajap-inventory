cbf52e49eb1b7f11a4e6d24de22aa5adb689a825
andrewbrianputosa <andrewbrianputosa@gmail.com>
2024-05-07 11:41:51 +0800

    test

--- src/main/java/com/cajap/inventory/InventoryApplication.java
+++ src/main/java/com/cajap/inventory/InventoryApplication.java
@@ -12,7 +12,7 @@ public class InventoryApplication {
 
 	public static void main(String[] args)  throws IOException {
 		SpringApplication.run(InventoryApplication.class, args);
-		Logger logger = Logger.getLogger("test test test");
+		Logger logger = Logger.getLogger("test");
 //		logger(Level.INFO,"test");
 	}
 
