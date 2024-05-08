6c9a5b404a532e2034f353b85e5e666f8b77cb01
andrewbrianputosa <andrewbrianputosa@gmail.com>
2024-05-08 17:03:51 +0800

    Clean code

--- src/main/java/com/cajap/inventory/InventoryApplication.java
+++ src/main/java/com/cajap/inventory/InventoryApplication.java
@@ -1,19 +1,13 @@
 package com.cajap.inventory;
 
-import org.slf4j.event.Level;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.IOException;
-//import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class InventoryApplication {
 
-	public static void main(String[] args)  throws IOException {
+	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
-		Logger logger = Logger.getLogger("test");
-//		logger(Level.INFO,"test");
 	}
 
 
